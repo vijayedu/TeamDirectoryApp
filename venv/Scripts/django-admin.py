a3674505b5f0ba447359570073ab216dcf0fce95
Vijay <itsvijay111@gmail.com>
2017-01-18 10:33:25 -0600

    Second Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\con-sat2vk\documents\codeschool\teamdirectoryapp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
